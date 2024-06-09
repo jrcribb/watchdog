90355a79c2ca4ce6b8fd53e77f0081329e14ae7d
jrcribb <jorge.cribb@gmail.com>
2024-06-09 18:47:33 -0300

    Comentarios REM en archivo

--- logwatchdog/test-folder/logwatchdog.cmd
+++ logwatchdog/test-folder/logwatchdog.cmd
@@ -1,2 +1,4 @@
+REM Se debe ejecutar estando en la carpeta test-folder del proyecto
+REM Usar el shell de windows
 call ..\..\environ.cmd
 java -jar logwatchdog-1.0-SNAPSHOT-jar-with-dependencies.jar 
